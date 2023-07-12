b2a9dabc08c82bbf9d7dc06b65dd1e14d07cfb4d
skutner <sava.dumitru.daniel@gmail.com>
2023-07-12 17:17:34 +0300

    axiologic-pla/dev#364 added lightDB component in apihub

--- components/lightDB/index.js
+++ components/lightDB/index.js
@@ -1,14 +1,36 @@
 const logger = $$.getLogger("lightDB", "apihub");
+const httpWrapper = require("../../libs/http-wrapper/src/httpUtils");
 function LightDB(server) {
     const lokiEnclaveFacadeModule = require("loki-enclave-facade");
     const createLightDBServerInstance = lokiEnclaveFacadeModule.createLightDBServerInstance;
-    createLightDBServerInstance(8081, server.rootFolder, "localhost", (err) => {
+    const HOST = "localhost";
+    const PORT = 8081;
+    const lightDBServerInstance = createLightDBServerInstance(PORT, server.rootFolder, HOST, (err) => {
         if (err) {
             return logger.error(err);
         }
 
         logger.info(`LightDB server started on port 8081`);
     });
+    const httpAPI = require("opendsu").loadAPI("http");
+    server.put("/executeLightDBCommand", httpWrapper.bodyParser);
+
+    server.put("/executeLightDBCommand", (req, res) => {
+        const url = `http://${HOST}:${PORT}/executeCommand`;
+        httpAPI.doPut(url, req.body, (err, response) => {
+            if (err) {
+                res.statusCode = 500;
+                logger.error(`Error while executing command ${JSON.parse(req.body).commandName}`, err);
+                res.write(err.message);
+                return res.end();
+            }
+
+            res.statusCode = 200;
+            res.write(response);
+            res.end();
+        });
+    })
+
 }
 
 module.exports = LightDB;
