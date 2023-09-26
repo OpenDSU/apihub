2829766d923e5060723fab1720b8d8a3bc50cb88
skutner <sava.dumitru.daniel@gmail.com>
2023-09-26 12:19:17 +0300

    axiologic-pla/dev#572 set initialURL when logoutCookie=true

--- middlewares/oauth/lib/OauthMiddleware.js
+++ middlewares/oauth/lib/OauthMiddleware.js
@@ -214,7 +214,10 @@ function OAuthMiddleware(server) {
             res.statusCode = 403;
             const loginUrl = oauthConfig.client.postLogoutRedirectUrl;
             const returnHtml ="<html>" +
-              `<body>We apologize for the inconvenience. The automated login attempt was unsuccessful. You can either <a href=\"${loginUrl}\">retry the login</a> or if the issue persists, please restart your browser.</body>` +
+              `<body>We apologize for the inconvenience. The automated login attempt was unsuccessful. 
+                    You can either <a href=\"${loginUrl}\">retry the login</a> or if the issue persists, please restart your browser.
+                    <script>sessionStorage.setItem('initialURL', window.location.href);</script>
+                </body>` +
               "</html>";
 
             return res.end(returnHtml);
