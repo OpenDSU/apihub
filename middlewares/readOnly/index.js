6cae67c07f49523629eebaf62e9c9dfe176779fe
skutner <sava.dumitru.daniel@gmail.com>
2024-02-08 10:03:28 +0200

    Refactor readOnly middleware code formatting
    Improved the readability of the 'readOnly' middleware used in the ApiHUB server of the DSU SDK by reformatting the function declaration lines and indentations. No functional changes were made, but the revised version is more adherent to standard JS coding style for better maintainability.

--- middlewares/readOnly/index.js
+++ middlewares/readOnly/index.js
@@ -5,42 +5,42 @@ fs = require(fs);
 
 let readOnly = false;
 
-module.exports = function(server){
-  let config = server.config;
-  let readOnlyFlag = config.readOnlyFile || "readonly";
-  let interval  = config.readOnlyInterval || 60*1000;
-  let rootStorage = path.resolve(config.storage);
-  readOnlyFlag = path.resolve(rootStorage, readOnlyFlag);
+module.exports = function (server) {
+    let config = server.config;
+    let readOnlyFlag = config.readOnlyFile || "readonly";
+    let interval = config.readOnlyInterval || 60 * 1000;
+    let rootStorage = path.resolve(config.storage);
+    readOnlyFlag = path.resolve(rootStorage, readOnlyFlag);
 
-  if(readOnlyFlag.indexOf(rootStorage) === -1){
-    console.warn(`ReadOnly flag location resolved outside of ApiHUB root folder. (${readOnlyFlag})`);
-  }
-
-  function checkReadOnlyFlag(){
-    fs.access(readOnlyFlag, fs.constants.F_OK, (err) => {
-      if(!err){
-        if(!readOnly){
-          console.info("Read only mode is activated.");
-          readOnly = true;
-        }
-      }else{
-        if(readOnly){
-          console.info("Read only mode is disabled.");
-          readOnly = false;
-        }
-      }
-    });
-  }
-
-  checkReadOnlyFlag();
-  setInterval(checkReadOnlyFlag, interval);
-
-  server.use("*", function(req, res, next){
-    if(readOnly && req.method !== "GET"){
-      res.statusCode = 405;
-      res.write("read only mode is active");
-      return res.end();
+    if (readOnlyFlag.indexOf(rootStorage) === -1) {
+        console.warn(`ReadOnly flag location resolved outside of ApiHUB root folder. (${readOnlyFlag})`);
     }
-    next();
-  });
+
+    function checkReadOnlyFlag() {
+        fs.access(readOnlyFlag, fs.constants.F_OK, (err) => {
+            if (!err) {
+                if (!readOnly) {
+                    console.info("Read only mode is activated.");
+                    readOnly = true;
+                }
+            } else {
+                if (readOnly) {
+                    console.info("Read only mode is disabled.");
+                    readOnly = false;
+                }
+            }
+        });
+    }
+
+    checkReadOnlyFlag();
+    setInterval(checkReadOnlyFlag, interval);
+
+    server.use("*", function (req, res, next) {
+        if (readOnly && req.method !== "GET") {
+            res.statusCode = 405;
+            res.write("read only mode is active");
+            return res.end();
+        }
+        next();
+    });
 }
