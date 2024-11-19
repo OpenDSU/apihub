db158f5989ffa6b72b7607bd0aef37a3241adc81
skutner <sava.dumitru.daniel@gmail.com>
2024-11-19 17:07:48 +0200

    Remove obsolete configuration files.
    Deleted outdated and unused configuration files from the SDK modules. This cleanup helps maintain a more organized and efficient codebase by eliminating redundant content.

--- libs/http-wrapper/src/index.js
+++
@@ -1,7 +0,0 @@
-const Client = require('./classes/Client');
-const Server = require('./classes/Server');
-const httpUtils = require('./httpUtils');
-const Router = require('./classes/Router');
-
-module.exports = {Server, Client, httpUtils, Router};
-
